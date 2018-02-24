3fd5652357bead4ab389480fb0fd3055945f5ace
EC2 Default User <ec2-user@ip-172-31-37-17.us-west-2.compute.internal>
2018-02-24 02:00:44 +0000

    use full fastq files

--- abseq.sh
+++ abseq.sh
@@ -3,13 +3,13 @@ DATA_DIR=/mnt/rawdata
 
 #  Files on Docker container
 YAML=/data/sample.yml
-READS_R1=/data/IgSeqBX1_S1_R1_001_first10k.fastq
-READS_R2=/data/IgSeqBX1_S1_R2_001_first10k.fastq
+READS_R1=/data/IgSeqBX1_S1_R1_001.fastq
+READS_R2=/data/IgSeqBX1_S1_R2_001.fastq
 R1_PRIMERS=/data/primers_R1.fasta
 R2_PRIMERS=/data/primers_R2_reverse_complement.fasta
 BARCODE_LENGTH=8
 SAMPLE_NAME=IgSeqBX1
-OUT_DIR=/data/presto-output_krista_primers_barcode8_R2_reverse
+OUT_DIR=/data/presto-output_krista_primers_barcode8_R2_reverse_full_dataset
 NPROC=16
 
 sudo docker run -v $DATA_DIR:/data:z kleinstein/immcantation:1.7.0 /data/presto-abseq.sh \
