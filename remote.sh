ae75b37175bd1bdf97d2859ba13a9aa369641fa0
olgabot <olga.botvinnik@gmail.com>
2018-04-06 15:19:06 -0700

    add comment

--- remote.sh
+++ remote.sh
@@ -1,2 +1,2 @@
-# Copy metadata and U-2 OS file 
+# Copy metadata and U-2 OS file
 aws s3 cp  --recursive  --exclude "*" --include "metadata*" --include "SRR629563*" s3://olgabot-seq2loc/fastq/rnaseq/ .
